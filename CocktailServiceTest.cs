c4e334387c94419ed0ca3bacbdf7cafeaa6b88e0
MITRAI\kjayasinghe <kjayasinghe@mitrai.com>
2021-09-20 02:57:44 +0530

    initial test project

---
+++ CocktailServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using api.Connector;
+using api.Models.Response;
+using api.Services;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace api.Test
+{
+    [TestClass]
+    public class CocktailServiceTest
+    {
+        [TestMethod]
+        public void Test_GetCocktailListByIngredient()
+        {
+            MockCocktailConnector mockTailConnector = new MockCocktailConnector();
+            ICocktailService CocktailService = new CocktailService(mockTailConnector);
+            var actual = CocktailService.GetCocktailListByIngredient("Gin");
+            var expected = new CocktailList();
+            Assert.AreEqual(expected,actual);
+
+        }
+
+        [TestMethod]
+        public void Test_GetRandomCocktaill()
+        {
+            MockCocktailConnector mockTailConnector = new MockCocktailConnector();
+            ICocktailService CocktailService = new CocktailService(mockTailConnector);
+            var actual = CocktailService.GetRandomCocktaill();
+            var expected = new Cocktail();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
